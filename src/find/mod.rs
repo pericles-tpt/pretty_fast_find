d8b757d9eb0e18695758258e325ea4b1e537ee3b
Pericles Telemachou <pericles@outlook.com.au>
2025-03-08 21:06:08 +1100

    Remove unused code from old project

--- src/find/mod.rs
+++ src/find/mod.rs
@@ -1,6 +1,6 @@
-use std::{collections::HashSet, io::Error};
-
-use crate::{utility::thread_from_root_new, walk::walk_search_until_limit};
+use std::{io::{Error, Write}, path::PathBuf};
+use rayon::iter::{ParallelIterator, IntoParallelRefMutIterator};
+use crate::walk::walk_search_until_limit;
 
 // t: 0 -> dir, 1 -> dir, 2 -> symlink, 3 -> other
 #[derive(Debug, Clone)]
@@ -22,38 +22,99 @@ pub fn find(target_substring: String, target_path: std::path::PathBuf, num_threa
         })
     }
     
-    let maybe_curr_scan: std::io::Result<Vec<FoundFile>> = thread_from_root_new(
-        target_path, 
-        HashSet::new(),
-        &target_substring, 
-        num_threads, 
-        thread_add_dir_limit, 
-        None, 
-        Some(walk_search_until_limit), 
-        |a, b| {
-            return a.p.cmp(&b.p);
-        },
-        maybe_filter,
-        sorted,
-        |a| {
-            a.p
-        }
-    );
+    let mut res: Vec<FoundFile> = Vec::new();
+
+    // Do first pass of thread_*_fn() on root to get multiple items
+    let mut initial_input = vec![target_path];
+    let maybe_initial_items = walk_search_until_limit(&target_substring, initial_input, &mut res, 2 * num_threads);
+    let Ok(mut paths_to_distribute) = maybe_initial_items else {return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to read root path: {:?}", maybe_initial_items.err())))};
     
-    if maybe_curr_scan.is_err() {
-        return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("Failed to do MT find: {:?}", maybe_curr_scan.err())))
+    // Interleave items in paths_to_distribute for better distribution
+    // Swap every 2nd item
+    let vc = (paths_to_distribute.len() / num_threads) + 1;
+    let mut paths_to_distribute_per_thread: Vec<Vec<PathBuf>> = vec![Vec::with_capacity(vc); num_threads];
+    let mut chunk_size = num_threads;
+    while paths_to_distribute.len() > 0 {
+        if chunk_size >= paths_to_distribute.len() {
+            chunk_size = paths_to_distribute.len();
+        }
+        let chunk: Vec<PathBuf> = paths_to_distribute.drain(0..chunk_size).collect();
+        
+        for j in 0..chunk.len() {
+            paths_to_distribute_per_thread[j].push(chunk[j].clone());
+        }
+    }
+    
+    loop {
+        let prs: Vec<(Vec<PathBuf>, Vec<FoundFile>)> = paths_to_distribute_per_thread.par_iter_mut().map(|p| {
+            let mut results: Vec<FoundFile> = Vec::new();
+            
+            let maybe_send_to_main = walk_search_until_limit(&target_substring, p.to_vec(), &mut results, thread_add_dir_limit);
+            
+            if maybe_filter.is_some() {
+                let filter_fn = maybe_filter.unwrap();
+                results = results.into_iter().filter(|it| {
+                    return filter_fn(it)
+                }).collect();
+            }
+            if maybe_send_to_main.is_err() {
+                return (vec![], results);
+            }
+            
+            if sorted {
+                return (maybe_send_to_main.unwrap(), results)
+            }
+            
+            let mut lines: Vec<String> = Vec::with_capacity(results.len());
+            for r in results {
+                lines.push(r.p);
+            }
+            if lines.len() > 0 {
+                let output_str = format!("{}\n", lines.join("\n"));
+                let res = std::io::stdout().write(output_str.as_bytes());
+            }
+            return (maybe_send_to_main.unwrap(), Vec::new());
+        }).collect();
+        
+        let split_pair: (Vec<Vec<PathBuf>>, Vec<Vec<FoundFile>>) = prs.into_iter().map(|(a, b)|(a, b)).unzip();
+        let mut new_results = split_pair.1.into_iter().flatten().collect();
+        paths_to_distribute = split_pair.0.into_iter().flatten().collect();
+        res.append(&mut new_results);
+        
+        if paths_to_distribute.len() == 0 {
+            break;
+        }
+        
+        // Split `paths_to_distribute` s.t. each thread operates on a (roughly) equal number of paths
+        let mut curr_num_threads = num_threads;
+        if paths_to_distribute.len() < curr_num_threads {
+            curr_num_threads = paths_to_distribute.len();
+        }
+        paths_to_distribute_per_thread = Vec::with_capacity(curr_num_threads);
+        
+        let min_paths_per_thread = paths_to_distribute.len() / curr_num_threads;
+        let mut rem_paths = paths_to_distribute.len() - (min_paths_per_thread * curr_num_threads);
+        while paths_to_distribute.len() > 0 {
+            let mut num_thread_paths = min_paths_per_thread;
+            if rem_paths > 0 {
+                num_thread_paths += 1;
+                rem_paths -= 1;
+            }
+            
+            let paths = paths_to_distribute.drain(0..num_thread_paths).collect();
+            paths_to_distribute_per_thread.push(paths);
+        }
     }
     
     if !sorted {
         return Ok(Vec::new())
     }
 
-    let mut curr_scan = maybe_curr_scan.unwrap();
-    curr_scan.sort_by(|a, b| {
+    res.sort_by(|a, b| {
         return a.p.cmp(&b.p);
     });
     
-    let ret = curr_scan.into_iter().map(|s|{
+    let ret = res.into_iter().map(|s|{
         return s.p
     }).collect();
 
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
 mod walk;
-mod save;
-mod scan;
 mod find;
-mod diff;
-mod report;
-mod utility;
 
 #[macro_use]
 extern crate lazy_static;
@@ -16,29 +11,20 @@ use std::io::Write;
 use std::num::ParseIntError;
 use std::{collections::HashMap, path::PathBuf};
 use std::env;
-use report::report_changes;
-use scan::scan;
 use find::find;
 use utility::MEGABYTE;
 
-const HELP_TEXT: &str = "usage: seye scan [OPTION]... [SCAN TARGET DIRECTORY] [SCAN SAVE FILE DIRECTORY]
-usage: seye report [SCAN TARGET DIRECTORY] [SCAN SAVE FILE DIRECTORY]
-usage: seye find [TARGET SUBSTRING] [ROOT FIND DIRECTORY]
+const HELP_TEXT: &str = "usage: pff [TARGET SUBSTRING] [ROOT FIND DIRECTORY]
 ------- Basic options -------
 --help      Print usage and this help message and exit.
-------- Scan options  -------
--p          Show performance statistics after scan
--md         (default: 0) Specify the minimum size difference to include in diffs, can specify one of: n, nK, nM or nG, e.g. 1M
+------- Find options  -------
 -t          (default: 0) Specify the number of threads (must be > 1, otherwise num_threads is set to 0)
 -tdl        (default: 256) Specify the minimum number of READDIRs per thread (if not enough dirs are found, this is ignored)";
-
-const MIN_MEMORY_LIMIT: usize = 10 * MEGABYTE;
 
 lazy_static! {
     static ref VALID_COMMAND_OPTIONS: HashMap<&'static str, HashSet<&'static str>> = {
         let mut map = HashMap::new();
-        map.insert("scan", HashSet::from_iter(vec!["-p", "-md", "-t", "-tdl"]));
-        map.insert("find", HashSet::from_iter(vec!["-t", "-tdl", "-h"]));
+        map.insert("find", HashSet::from_iter(vec!["-t", "-tdl", "-h", "-s"]));
         map
     };
 }
@@ -178,66 +164,14 @@ fn main() {
             let res = find(target_substring.clone(), target_pb, num_threads, thread_add_dir_limit, search_hidden);
             match res {
                 Ok(entries) => {
-                    let output_str = format!("{}\n", entries.join("\n"));
-                    let res = std::io::stdout().write(output_str.as_bytes());
-                    if res.is_err() {
-                        eprint!("failed to write `find` results to stdout: {:?}", res.err());
+                    if sorted {
+                        let output_str = format!("{}\n", entries.join("\n"));
+                        let res = std::io::stdout().write(output_str.as_bytes());
+                        if res.is_err() {
+                            eprint!("failed to write `find` results to stdout: {:?}", res.err());
+                        }
                     }
                 }
-                Err(e) => {
-                    eprintln!("error occured while reporting: {}", e);
-                }
-            }
-        }
-        "report" => {
-            if params.len() < 2 {
-                eprintln!("insufficient arguments for `scan`, expected at least [INPUT SCAN PATH] and [OUTPUT SCAN FILE PATH]");
-                return;
-            }
-            let optional_args: Vec<_> = params.iter().collect();
-            let num_args = optional_args.len();
-            
-            // Get input scan path
-            let maybe_target_str = params[num_args - 2];
-            let maybe_target_pb = validate_get_pathbuf(maybe_target_str);
-            if maybe_target_pb.is_err() {
-                eprintln!("invalid target path provided: {}", maybe_target_pb.err().unwrap());
-                return;
-            }
-            let target_pb = maybe_target_pb.unwrap();
-
-            // Get scan output path
-            let maybe_output_pb = validate_get_pathbuf(params[num_args - 1]);
-            if maybe_output_pb.is_err() {
-                eprintln!("invalid output scan path provided: {}", maybe_output_pb.err().unwrap());
-                return;
-            }
-            let mut output_pb = maybe_output_pb.unwrap();
-
-            // println!("Running scan of '{}', with {} threads and a {} memory limit", maybe_target_str, num_threads, get_shorthand_memory_limit(memory_limit));
-            
-            // Create `su` folder if it doesn't exist
-            let mut su_path = output_pb.clone();
-            su_path.push("su/");
-            let su_exists = std::fs::exists(&su_path);
-            if su_exists.is_err() {
-                eprintln!("failed to check if 'su' path exists: {:?}", su_exists.err());
-                return;
-            }
-            if !su_exists.unwrap() {
-                let res = std::fs::create_dir(&su_path);
-                if res.is_err() {
-                    eprintln!("failed to create 'su' path: {:?}", res.err());
-                    return;
-                }
-            }
-            if is_root {
-                output_pb = su_path;
-            }
-            
-            let res = report_changes(target_pb, output_pb);
-            match res {
-                Ok(()) => {}
                 Err(e) => {
                     eprintln!("error occured while reporting: {}", e);
                 }
@@ -274,72 +208,6 @@ fn eval_optional_args(cmd: &str, args: Vec<&&String>, show_perf_info: &mut bool,
         }
     
         match cmd {
-            "scan" => 'scan: {
-                // NO VALUE OPTIONS
-                let mut is_no_val_opt = true;
-                match a {
-                    "-p" => {
-                        *show_perf_info = true;
-                    }
-                    // "-r" => {
-                    //     *is_recursive = true;
-                    // }
-                    _ => {is_no_val_opt = false;}
-                }
-                if is_no_val_opt {
-                    break 'scan;
-                }
-
-                // ONE VALUE OPTIONS
-                i += 1;
-                if i >= args.len() {
-                    return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("missing additional argument for '{}' flag", a)));
-                }
-                match a {
-                    "-m" => {
-                        let maybe_memory_limit = get_bytes_from_arg(args[i]);
-                        if maybe_memory_limit.is_err() {
-                            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid memory limit argument, {}", maybe_memory_limit.err().unwrap())));
-                        }
-                        *memory_limit = maybe_memory_limit.unwrap();
-                        if *memory_limit < MIN_MEMORY_LIMIT {
-                            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("memory limit too low must be at least {}M", MIN_MEMORY_LIMIT / MEGABYTE)));
-                        }
-                    }
-                    "-md" => {
-                        let maybe_min_diff_bytes = get_bytes_from_arg(args[i]);
-                        if maybe_min_diff_bytes.is_err() {
-                            // Try to parse as u64 num bytes
-                            let maybe_min_diff_bytes_raw = args[i].parse::<i64>();
-                            if maybe_min_diff_bytes_raw.is_err() {
-                                return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, format!("invalid min diff bytes argument, failed to parse as shorthand (e.g. 10M, 1G, etc) or raw bytes (e.g. 1000)")));
-                            }
-                            *min_diff_bytes = maybe_min_diff_bytes_raw.unwrap();
-                        } else {
-                            *min_diff_bytes = maybe_min_diff_bytes.unwrap() as i64;
-                        }
-                    }
-                    "-t" => {
-                        let maybe_threads: Result<usize, ParseIntError> = args[i].parse();
-                        if maybe_threads.is_err() || maybe_threads.clone().unwrap() < 1 {
-                            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid thread argument, must be a non-negative integer"));
-                        }
-                        *num_threads = maybe_threads.unwrap();
-                    }
-                    "-tdl" => {
-                        let maybe_thread_add_dir_limit: Result<usize, ParseIntError> = args[i].parse();
-                        if maybe_thread_add_dir_limit.is_err() || maybe_thread_add_dir_limit.clone().unwrap() < 1 {
-                            return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "invalid thread add dir limit argument, must be a non-negative integer"));
-                        }
-                        *thread_add_dir_limit = maybe_thread_add_dir_limit.unwrap();
-                    }
-                    _ => {
-                        if before_directory_args {
-                            return Err(std::io::Error::new(std::io::ErrorKind::Other, format!("unimplemented parameter: {}, for command: {}", a, cmd)));
-                        }
-                    }
-                }
-            }
             "find" => 'find: {
                 // NO VALUE OPTIONS
                 let mut is_no_val_opt = true;
